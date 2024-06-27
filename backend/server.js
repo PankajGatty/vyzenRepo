176defa71e4c2c203fa932de634920639dcfc8a9
PankajGatty <pankajgatty022000@gmail.com>
2024-06-28 01:34:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors({
   credentials : true,
   origin: ["https://vyzen-repo-frontend.vercel.app"],
-  methods:['POST","GET"]
+  methods:['POST',"GET"]
 }))
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
