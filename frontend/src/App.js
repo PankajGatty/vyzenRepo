1217dbf13ebc972928969b5dc6ff48817af2aa86
PankajGatty <pankajgatty022000@gmail.com>
2024-06-28 03:59:41 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
         <Route path="/page9" element={<><MainPages/><Page9/></>}/>
         <Route path="/page10" element={<><MainPages/><Page10/></>}/>
         <Route path="/page11" element={<><MainPages/><Page11/></>}/>
-        <Route path="/Finalpage" element={<><Finalpage/></>}/>
+        <Route path="/final" element={<><Finalpage/></>}/>
 
       </Routes>
     </BrowserRouter>        );
