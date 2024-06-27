bc5857e3564afa977d6831a40be1594e8e607cde
PankajGatty <pankajgatty022000@gmail.com>
2024-06-28 01:31:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,15 @@ app.use(bodyParser.json());
 
 app.use(cors({
   credentials : true,
-  origin: ["https://vyzen-repo-frontend.vercel.app"]
+  origin: ["https://vyzen-repo-frontend.vercel.app"],
+  methods:['POST","GET"]
 }))
-
-app.use(express.static('public'));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.use(express.json());
 
 app.use('/api',require('./routes/router'));
